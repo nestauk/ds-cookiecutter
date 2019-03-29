4ffd43659727bdc6cee9643808078ba7a09a16c6
bishax <alex.bishop@nesta.org.uk>
2019-03-29 13:49:58 +0000

    Added git filter that strips jupyter notebooks of output. This is installed by default by the cookiecutter. A `make git` command is also available that installs the filter for any user who forks/clones the repository

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+# git filter to strip notebooks of output
+config="[filter \"nbstrip_full\"]\n clean = \"jq --indent 1 '(.cells[] | select(has(\\\"outputs\\\")) | .outputs) = []  | (.cells[] | select(has(\\\"execution_count\\\")) | .execution_count) = null  | .metadata = {\\\"language_info\\\": {\\\"name\\\": \\\"python\\\", \\\"pygments_lexer\\\": \\\"ipython3\\\"}} | .cells[].metadata = {} '\"\n smudge = cat\n required = true\n "
+
+git init  # Create git repo
+echo -e $config >> .git/config  # Copy git filter
+
+git add .
+git commit -am"Setup Nesta Data Science cookiecutter"
+
+
