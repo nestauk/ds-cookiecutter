70079ac4c44539ad2b284e7c06b9da5e9f345cc0
bishax <alex.bishop@nesta.org.uk>
2019-04-24 15:07:18 +0100

    Added module logging

---
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/__init__.py
@@ -0,0 +1,20 @@
+import logging
+import logging.config
+import yaml
+from pathlib import Path
+
+
+# Define project base directory
+project_dir = Path(__file__).resolve().parents[1]
+
+# Define log output locations
+info_out = str(project_dir / 'info.log')
+error_out = str(project_dir / 'errors.log')
+
+# Read log config file
+with open(project_dir / 'logging.yaml', 'rt') as f:
+    config = yaml.safe_load(f.read())
+logging.config.dictConfig(config)
+
+# Define module logger
+logger = logging.getLogger(__name__)
