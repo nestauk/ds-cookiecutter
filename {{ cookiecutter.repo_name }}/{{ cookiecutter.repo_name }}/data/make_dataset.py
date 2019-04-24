326616b7310b3cee0d6807a4ce6a3f1572a225de
bishax <alex.bishop@nesta.org.uk>
2019-04-24 16:18:47 +0100

    Added better template function for make_dataset.py

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/data/make_dataset.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/data/make_dataset.py
@@ -3,23 +3,38 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+# Important to import the module to configure logging
+import {{ cookiecutter.repo_name }}
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+@click.argument('arg', type=str)
+@click.option('-o', '--opt', default=None, type=int)
+def main(arg, opt):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
+
+    Usage: `python make_dataset.py arg -o opt`
+
+    Args:
+        arg (str):
+            Required command line argument.
+        opt (int, optional):
+            Command line option.
+            Set with `-o` or `--opt`.
+            Defaults to None.
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+
+    try:
+        msg = f'making final data set from raw data with: arg={arg}; opt={opt}'
+        logger.info(msg)
+    except (Exception, KeyboardInterrupt) as e:
+        logger.exception('make_dataset ({arg}, {opt}) failed', stack_info=True)
+        raise e
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
 
