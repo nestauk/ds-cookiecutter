77357ebafc0279f81a45069ec4542b2881c03b95
bishax <alex.bishop@nesta.org.uk>
2021-01-21 12:22:55 +0000

    Generate and configure git repo properly given new structure and hooks.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,15 +1,17 @@
 #!/bin/bash 
 
-# git filter to strip notebooks of output
-config="[filter \"nbstrip_full\"]\n clean = \"jq --indent 1 '(.cells[] | select(has(\\\"outputs\\\")) | .outputs) = []  | (.cells[] | select(has(\\\"execution_count\\\")) | .execution_count) = null  | .metadata = {\\\"language_info\\\": {\\\"name\\\": \\\"python\\\", \\\"pygments_lexer\\\": \\\"ipython3\\\"}} | .cells[].metadata = {} '\"\n smudge = cat\n required = true\n "
+# Create git repo
+git init
 
-git init  # Create git repo
-echo -e $config >> .git/config  # Copy git filter
+# Setup empty master and dev
+git checkout -b master
+git commit --allow-empty -m "Initial commit."
+git checkout -b dev
 
+# Add cookiecutter on new branch
+git checkout -b 0_setup_cookiecutter  # Issue numbers don't start at zero so this won't conflict
 git add .
-git add data/raw -f
-git add data/interim -f
-git add data/processed -f
-git commit -am"Setup Nesta Data Science cookiecutter"
+git commit -am "Setup Nesta Data Science cookiecutter"
 
-
+# Setup git hooks
+make git
