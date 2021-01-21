a5fd128beee2da948b60b545bee8ed0a0f8e8b9f
bishax <alex.bishop@nesta.org.uk>
2021-01-21 12:22:27 +0000

    Improve git setup: forbid `main` commit and check for git hook first

--- {{ cookiecutter.repo_name }}/bin/git_setup.sh
+++ {{ cookiecutter.repo_name }}/bin/git_setup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-
 # Configure git hook
 chmod +x .githooks/pre-commit
-ln -s $PWD/.githooks/pre-commit .git/hooks/pre-commit
+[ ! -f .git/hooks/pre-commit ] && ln -s $PWD/.githooks/pre-commit .git/hooks/pre-commit
