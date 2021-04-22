b38648b489579343d3ed5d012a29f88be214bb4a
bishax <alex.bishop@nesta.org.uk>
2021-04-22 10:15:36 +0100

    main __init__ refactoring

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/__init__.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/__init__.py
@@ -1,11 +1,14 @@
+"""{{ cookiecutter.repo_name }}."""
 import logging
 import logging.config
-import yaml
 from pathlib import Path
 from typing import Optional
 
+import yaml
 
-def get_log_config(file_path: Path) -> Optional[dict]:
+
+def get_yaml_config(file_path: Path) -> Optional[dict]:
+    """Fetch yaml config and return as dict if it exists."""
     if file_path.exists():
         with open(file_path, "rt") as f:
             return yaml.load(f.read(), Loader=yaml.FullLoader)
@@ -14,21 +17,16 @@ def get_log_config(file_path: Path) -> Optional[dict]:
 # Define project base directory
 project_dir = Path(__file__).resolve().parents[1]
 
-# Define log output locations
-info_out = str(project_dir / "info.log")
-error_out = str(project_dir / "errors.log")
 
 # Read log config file
-log_config_file = project_dir / "logging.yaml"
-logging_config = get_log_config(log_config_file)
-if logging_config:
-    logging.config.dictConfig(logging_config)
+_log_config_path = Path("config/logging.yaml")
+_logging_config = get_yaml_config(_log_config_path)
+if _logging_config:
+    logging.config.dictConfig(_logging_config)
 
 # Define module logger
 logger = logging.getLogger(__name__)
 
 # Model config
-with open(
-    project_dir / "{{ cookiecutter.repo_name }}" / "model_config.yaml", "rt"
-) as f:
-    config = yaml.safe_load(f.read())
+_model_config_path = Path("config/logging.yaml")
+config = get_yaml_config(_model_config_path)
