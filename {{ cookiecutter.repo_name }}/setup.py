546fec12f79df00caddfe006412f63dab0315dba
bishax <alex.bishop@nesta.org.uk>
2021-04-26 15:43:36 +0100

    Migrate to simple conda env + requirements.txt + requirements_dev.txt

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -2,9 +2,19 @@
 from setuptools import find_packages
 from setuptools import setup
 
+
+def read_lines(path):
+    """Read lines of `path`."""
+    with open(path) as f:
+        return f.read().splitlines()
+
+
 setup(
     name="{{ cookiecutter.repo_name }}",
-    packages=find_packages(),
+    long_description=open("README.md").read(),
+    install_requires=read_lines("requirements.txt"),
+    extras_require={"dev": read_lines("requirements_dev.txt")},
+    packages=find_packages(exclude=["docs"]),
     version="0.1.0",
     description="{{ cookiecutter.description }}",
     author="{{ cookiecutter.author_name }}",
