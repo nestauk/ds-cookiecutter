3fc73bf8a50ce3ec3f2650be1f11c376a47d0138
bishax <alex.bishop@nesta.org.uk>
2021-06-03 17:00:55 +0100

    ADD: shellcheck and safer defaults

--- {{ cookiecutter.repo_name }}/bin/conda_activate.sh
+++ {{ cookiecutter.repo_name }}/bin/conda_activate.sh
@@ -1,6 +1,10 @@
-function conda_activate() {
-    source .env.shared \
-     && source $(conda info --base)/etc/profile.d/conda.sh  \
-     && conda activate $PROJECT_NAME \
-     || source activate $PROJECT_NAME;
+#!/bin/bash
+set -euo pipefail
+
+# Avoid unbound variable error
+export PS1=${PS1:-}
+
+conda_activate() {
+    source "$(conda info --base)/etc/profile.d/conda.sh"  \
+    && conda activate "$PROJECT_NAME"
 }
--- {{ cookiecutter.repo_name }}/bin/create_bucket.sh
+++ {{ cookiecutter.repo_name }}/bin/create_bucket.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 set -euo pipefail
 
-source "$PWD/.env.shared"
+# shellcheck source=../.env.shared
+source "$(dirname "$0")/../.env.shared"
 
 function create_bucket() {
     echo "Attempting creation of bucket $BUCKET ...";
     aws s3api create-bucket\
-     --bucket $BUCKET\
-     --region eu-west-2\
-     --create-bucket-configuration\
-     'LocationConstraint=eu-west-2';
+    --bucket "$BUCKET"\
+    --region eu-west-2\
+    --create-bucket-configuration\
+    'LocationConstraint=eu-west-2';
 }
 
 function make_bucket_private() {
     echo "Configuring $BUCKET to block all public access...";
     aws s3api put-public-access-block\
-     --bucket $BUCKET\
-     --public-access-block-configuration\
-     "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true";
+    --bucket "$BUCKET"\
+    --public-access-block-configuration\
+    "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true";
 }
 
 create_bucket && make_bucket_private || echo "ERROR: Bucket creation failed!"
