1c063ed366e19bf6019139630f656fd172cfbf81
bishax <alex.bishop@nesta.org.uk>
2021-06-04 16:10:03 +0100

    67 guard against whitespace (#71)
    Incidental improvements:
    * shellcheck and safer defaults
    * simpler extraction of project description from setup.py
    * name conda env after REPO_NAME not PROJECT_NAME

--- {{ cookiecutter.repo_name }}/bin/create_repo.sh
+++ {{ cookiecutter.repo_name }}/bin/create_repo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-set -eo pipefail
+set -euo pipefail
 
-source "$PWD/.env.shared"
-source "$PWD/bin/conda_activate.sh"
+# # shellcheck source=./conda_activate.sh
+# source "$(dirname "$0")/conda_activate.sh"
 
 # Fetch description of package (setup.py and conda make this horrible)
-DESCRIPTION=$(conda_activate && pip show $PROJECT_NAME | grep Summary | cut -d " " -f2-)
+DESCRIPTION=$(python "$(dirname "$0")/../setup.py" --description)
 
 # Create
-gh repo create $GITHUB_ACCOUNT/$PROJECT_NAME --$PROJECT_OPENNESS -d "$DESCRIPTION" -y
+gh repo create "$GITHUB_ACCOUNT/$PROJECT_NAME" --"$PROJECT_OPENNESS" -d "$DESCRIPTION" -y
 
 # push initial branches
 git push --all
@@ -17,7 +17,7 @@ git push --all
 # - default branch to `dev`
 # - squash merge as only option
 gh api -X PATCH\
- -F default_branch=dev\
- -F allow_merge_commit=false\
- -F allow_rebase_merge=false\
-  repos/$GITHUB_ACCOUNT/$PROJECT_NAME --silent
+-F default_branch=dev\
+-F allow_merge_commit=false\
+-F allow_rebase_merge=false\
+"repos/$GITHUB_ACCOUNT/$PROJECT_NAME" --silent
