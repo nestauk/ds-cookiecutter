2e842151f55d62ec08aa1aecd69ff62d52313746
bishax <alex.bishop@nesta.org.uk>
2021-06-14 09:35:17 +0100

    Don't commit examples

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -8,10 +8,16 @@ git checkout -b master -q
 git commit --allow-empty -m "Initial commit." -q
 git checkout -b dev -q
 
-# Add cookiecutter on new branch
+## Add cookiecutter on new branch
 # Issue numbers don't start at zero so this won't conflict
+export REPO_NAME="{{ cookiecutter.repo_name }}"
 git checkout -b 0_setup_cookiecutter  -q
-git add . 
+git add .
+# Don't add example flows
+git restore --staged \
+"$REPO_NAME/pipeline/example/example_flow.py" \
+"$REPO_NAME/getters/example.py" \
+"$REPO_NAME/config/pipeline/example/example_flow_local.yaml"
 git commit -am "Setup Nesta Data Science cookiecutter" -q
 
 echo Configured git repo at $(pwd)
@@ -19,7 +25,7 @@ echo Configured git repo at $(pwd)
 # Do everything if configured
 if [ {{ cookiecutter.auto_config }} == "true" ]
 then
- make init
+    make init
 else
- echo "You did not select auto_config, please enter the project directory & run 'make init' manually"
+    echo "You did not select auto_config, please enter the project directory & run 'make init' manually"
 fi
