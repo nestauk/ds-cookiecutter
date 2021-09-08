783449f9d759bc4affb7b1ee708f8cbee180d4b8
bishax <alex.bishop@nesta.org.uk>
2021-09-08 16:36:45 +0100

    pre-commit run all files

--- {{ cookiecutter.repo_name }}/.cookiecutter/scripts/conda_activate.sh
+++ {{ cookiecutter.repo_name }}/.cookiecutter/scripts/conda_activate.sh
@@ -5,7 +5,7 @@ conda_activate() {
 
     # Avoid unbound variable error
     export PS1=${PS1:-}
-    
+
     # shellcheck disable=SC1091
     source "$(conda info --base)/etc/profile.d/conda.sh"  \
     && conda activate "$1"
--- {{ cookiecutter.repo_name }}/.cookiecutter/scripts/install_metaflow_aws.sh
+++ {{ cookiecutter.repo_name }}/.cookiecutter/scripts/install_metaflow_aws.sh
@@ -7,20 +7,20 @@ function get_metaflow_config() {
 
     # Fetch research daps key
     aws s3 cp s3://nesta-production-config/research_daps.key .
-    
+
     # Clone research daps
     cd /tmp
     \rm -rf /tmp/research_daps
     git clone git@github.com:nestauk/research_daps.git
     cd research_daps
-    
+
     # Unencrypt research daps
     git-crypt unlock "$PROJECT_DIR/research_daps.key" &> /dev/null
-    
+
     # Copy metaflow config
     mkdir -p "$HOME/.metaflowconfig"
     cp research_daps/config/metaflowconfig/config.json "$HOME/.metaflowconfig/config_ds-cookiecutter.json"
-    
+
     # Clean up
     \rm -rf /tmp/research_daps
     cd "$PROJECT_DIR"
