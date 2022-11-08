f1fc951cc4ead398400862d6df438840f977b53c
Emily Bicks <emilybicks@NESTA-CQ6PWN4772.home>
2022-11-08 10:06:23 +0000

    removing git creation steps from post gen

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Create git repo
-git init -q
-
-# Setup empty master and dev
-git checkout -b master -q
-git commit --allow-empty -m "Initial commit." -q
-git checkout -b dev -q
-
 # Add cookiecutter on new branch
 # Issue numbers don't start at zero so this won't conflict
 git checkout -b 0_setup_cookiecutter  -q
