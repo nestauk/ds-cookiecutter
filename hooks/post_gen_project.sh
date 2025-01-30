a5af35ba0318f85d1853e947a8691226ce2a8772
Seth Caldwell <caldwellst@gmail.com>
2025-01-30 10:42:12 +0000

    Add initial update to post_gen_project.sh for testing

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -8,6 +8,9 @@ if [ -n "$path" ] && [ -e "$path" ]; then
     rm "$path"
 fi
 
+# Permit auto-loading of .envrc
+direnv allow
+
 # Create git repo
 git init -q
 
@@ -17,7 +20,30 @@ git add .
 git commit -am "Setup Nesta Data Science cookiecutter" -q
 git checkout -b dev -q
 
-echo "Configured git repo at $(pwd), please enter the project directory & run 'make install'"
+# Create remote repository 
+REPO_NAME="{{ cookiecutter.project_slug }}"
+OPENNESS="{% if cookiecutter.openness == 'private' %}--private{% else %}--public{% endif %}"
+gh repo create "$REPO_NAME" $OPENNESS --source=. --remote=origin --push
 
-# Permit auto-loading of .envrc
-direnv allow
+# Transfer the repository to the organisation
+gh repo transfer "nesta-cookiecutter-test/$REPO_NAME" --confirm
+
+# Create issue on github_support
+
+ISSUE_TITLE="Configure repository: $REPO_NAME"
+ISSUE_BODY="Repository setup requested for $REPO_NAME
+
+Please configure:
+- Branch protection rules
+- Team access
+- Any other standard Nesta repository settings
+
+Thank you!"
+
+gh issue create --repo nestauk/github_support --title "$ISSUE_TITLE" --body "$ISSUE_BODY"
+
+ISSUE_NUMBER=$(gh issue create --repo nestauk/github_support --title "$ISSUE_TITLE" --body "$ISSUE_BODY" --json number -q .number)
+
+echo "Repository created and transferred: https://github.com/nesta-cookiecutter-test/$REPO_NAME"
+echo "Support issue created: https://github.com/nesta-cookiecutter-test/github_support/issues/$ISSUE_NUMBER"
+echo "Configured git repo at $(pwd), please enter the project directory & run 'make install'"
