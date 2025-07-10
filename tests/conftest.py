ba4df77a17f82dbc0efadcf57a3f0f8d03794d57
HarrisonWilde <harry.wilde@nesta.org.uk>
2025-07-10 14:47:35 +0100

    Another attempt

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ base_args = {
     "project_name": "Nesta Test Cookie",
     "module_name": "nestatestcookie",
     "openness": "public",
-    "python_version": "3.12",
+    "python_version": "3.13",
     "autosetup": "yes",
 }
 test_params = [
@@ -27,7 +27,7 @@ def get_test_id(test_param: dict) -> str:
 
 
 @pytest.fixture(scope="class", params=test_params, ids=get_test_id)
-def default_baked_project(tmpdir_factory: pytest.TempdirFactory, request: pytest.FixtureRequest):
+def default_baked_project(tmpdir_factory: pytest.TempdirFactory, request: pytest.FixtureRequest):  # noqa: ANN201
     temp = tmpdir_factory.mktemp("data-project")
     out_dir = Path(temp).resolve()
 
